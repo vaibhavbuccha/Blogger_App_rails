eaf60fffcba47747816db31886a168a7b17c1503
vaibhav jain <jainvaibhav415@gmail.com>
2020-03-23 14:04:34 +0530

    blogger

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,20 +3,38 @@ class BlogsController < ApplicationController
   end
 
   def show
+    if logged_in?
+      user = current_user
+      @blog = Blog.where(user_id: user.id).order("created_at DESC")
+      # abort @blog.inspect
+    else
+      flash[:danger] = "Please login first"
+      redirect_to login_path
+    end
   end
 
   def new
-    @blog = Blog.new
+    if logged_in?
+      @blog = Blog.new
+    else
+      flash[:danger] = "Please login first"
+      redirect_to login_path
+    end
   end
 
   def create
-    @blog = Blog.new(blog_params)
-    if @blog.save
-      flash[:success] = "Blog added successfully..."
-      redirect_to root_path
+    if logged_in?
+      @blog = Blog.new(blog_params)
+      if @blog.save
+        flash[:success] = "Blog added successfully..."
+        redirect_to root_path
+      else
+        flash[:danger] = "Somthing wents wrong!"
+        redirect_to(new)
+      end
     else
-      flash[:danger] = "Somthing wents wrong!"
-      redirect_to(new)
+      flash[:danger] = "Please login first"
+      redirect_to login_path
     end
   end
 
@@ -25,6 +43,19 @@ class BlogsController < ApplicationController
 
   def delete
   end
+
+  def destroy
+    if logged_in?
+      @blog = Blog.find(params[:blog][:id])
+      @blog.destroy 
+      flash[:success] = "Blog '#{@blog.title}' deleted successfully.... "
+      redirect_to(root_path)
+    else
+      flash[:danger] = "Please login first"
+      redirect_to login_path
+    end
+  end
+
 
   private
   def blog_params
